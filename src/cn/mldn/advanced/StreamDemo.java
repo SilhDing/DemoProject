af0c5dac328377ea3e0fde88d10dddf7aa658312
SilhDing <dingyihang1994@gmail.com>
2018-06-28 20:44:55 +0800

    edit 38

--- src/cn/mldn/advanced/StreamDemo.java
+++ src/cn/mldn/advanced/StreamDemo.java
@@ -1,12 +1,14 @@
 package cn.mldn.advanced;
 
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * @Description: Despription
+ * @Description: stream和mapreduce
  * @ProjectName: DemoProject
  * @Package: cn.mldn.advanced
  * @Author: Yihang Ding
@@ -16,6 +18,31 @@ import java.util.stream.Stream;
  * @UpdateRemark: The modified content
  * @Version: 1.0
  */
+
+class ShopCar{
+    private String pname;
+    private double price;
+    private int amount;
+
+    public ShopCar(String pname, double price, int amount) {
+        this.pname = pname;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
+
 public class StreamDemo {
     public static void main(String[] args) {
         List<String> all = new ArrayList<>();
@@ -24,6 +51,8 @@ public class StreamDemo {
         all.add("yeah");
         all.add("yihang");
         all.forEach(System.out::println);
+
+        //一个stream只能操作一次
 
         Stream<String> stream = all.stream();
         // System.out.println(stream.count());
@@ -41,5 +70,38 @@ public class StreamDemo {
         Stream<String> stream_4 = all.stream();
         List<String> newAll_4 = stream_4.skip(2).limit(2).collect(Collectors.toList());
         newAll_4.forEach(System.out::println);
+
+        Stream<String> stream_5 = all.stream();
+        if (stream_5.anyMatch((x) -> x.contains("hang"))) {
+            System.out.println("data exists!");
+        }
+
+        Stream<String> stream_6 = all.stream();
+        Predicate<String> p1 = (x) -> x.contains("e");
+        Predicate<String> p2 = (x) -> x.contains("a");
+        if (stream_6.anyMatch(p1.and(p2))) {
+            System.out.println("data exists!");
+        }
+
+        // mapreduce
+        List<ShopCar> car = new ArrayList<>();
+        car.add(new ShopCar("book", 12, 8));
+        car.add(new ShopCar("paper", 0.4, 332));
+        car.add(new ShopCar("tv", 3232.9, 2));
+        car.add(new ShopCar("umbrella", 39, 10));
+        double s = car.stream().map((x) -> x.getAmount() * x.getPrice()).reduce((sum, m)->sum + m).get();
+        System.out.println("total cost: " + s);
+
+        DoubleSummaryStatistics dss = car.stream().
+                mapToDouble((sc)->(sc.getAmount()*sc.getPrice())).summaryStatistics();
+        System.out.println("all amount: " + dss.getCount());
+        System.out.println("all cost: " + dss.getSum());
+        System.out.println("average cost: " + dss.getAverage());
+        System.out.println("max cost: " + dss.getMax());
+        System.out.println("min cost: " + dss.getMin());
+
+
+
+
     }
 }
