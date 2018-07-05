3bb4572c103f9bb4fd58c8aa139e491667e1cd60
SilhDing <dingyihang1994@gmail.com>
2018-07-05 19:39:24 +0800

    edit 44

---
+++ src/cn/mldn/util/Queue.java
@@ -0,0 +1,15 @@
+package cn.mldn.util;
+
+/**
+ * @Description: FIFO queue based on linked-list
+ * @ProjectName: DemoProject
+ * @Package: cn.mldn.util
+ * @Author: Yihang Ding
+ * @CreateDate: 2018/7/5 19:38
+ * @UpdateUser: Yihang Ding
+ * @UpdateDate: 2018/7/5 19:38
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class Queue {
+}
