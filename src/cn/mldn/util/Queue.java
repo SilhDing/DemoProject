194f6443b023b1a8374236afa76cf4c42aeaa73b
SilhDing <dingyihang1994@gmail.com>
2018-07-26 19:07:08 +0800

    edit 56

--- src/cn/mldn/util/Queue.java
+++ src/cn/mldn/util/Queue.java
@@ -1,5 +1,7 @@
 package cn.mldn.util;
 
+import java.util.Iterator;
+
 /**
  * @Description: FIFO queue based on linked-list
  * @ProjectName: DemoProject
@@ -11,7 +13,7 @@ package cn.mldn.util;
  * @UpdateRemark: The modified content
  * @Version: 1.0
  */
-public class Queue<Item> {
+public class Queue<Item> implements Iterable<Item> {
     private Node first;
     private Node last;
     private int N;
@@ -49,5 +51,25 @@ public class Queue<Item> {
         if (isEmpty()) last = null;
         N -- ;
         return item;
+    }
+
+    public Iterator<Item> iterator() {
+         return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {};
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 }
