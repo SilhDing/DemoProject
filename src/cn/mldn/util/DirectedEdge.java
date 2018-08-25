064817d79882cfa20f340bb778466039ce4a5c09
SilhDing <dingyihang1994@gmail.com>
2018-08-25 12:21:16 -0400

    edit 72

---
+++ src/cn/mldn/util/DirectedEdge.java
@@ -0,0 +1,42 @@
+package cn.mldn.util;
+
+/**
+ * @Description: Despription
+ * @ProjectName: DemoProject
+ * @Package: cn.mldn.util
+ * @Author: Yihang Ding
+ * @CreateDate: 2018/8/25 12:09
+ * @UpdateUser: Yihang Ding
+ * @UpdateDate: 2018/8/25 12:09
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class DirectedEdge {
+
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d->%d",v,w,weight);
+    }
+}
